86e54cf90707b5a48b2e091461e5bcf0412cda85
McKrackn <72325248+McKrackn@users.noreply.github.com>
2021-12-02 19:54:22 +0100

    ML working

--- Assets/Scripts/PlayerAgent.cs
+++ Assets/Scripts/PlayerAgent.cs
@@ -9,15 +9,12 @@ public class PlayerAgent : Agent
 {
 
     Rigidbody2D rBody;
-    //void Start()
-    //{
-    //    rBody = GetComponent<Rigidbody2D>();
-    //}
 
     [SerializeField] public Transform Target;
     [SerializeField] public Transform Death;
 
     CharacterState characterState;
+    private float distance2Target;
 
     private void Start()
     {
@@ -27,7 +24,6 @@ public class PlayerAgent : Agent
 
     public override void OnEpisodeBegin()
     {
-        System.Console.WriteLine("d");
         float[] possiblePositionX = new float[] { -3.5f, 3.5f };
         int randomIndex = Random.Range(0, possiblePositionX.Length);
         float positionX = possiblePositionX[randomIndex];
@@ -36,18 +32,7 @@ public class PlayerAgent : Agent
         Death.transform.position = (randomIndex == 0) ? new Vector3(possiblePositionX[1], 0, 0) : new Vector3(possiblePositionX[0], 0, 0);
 
         transform.position = new Vector3(0, 0, 0);
-        //if (this.transform.localPosition.y < 0)
-        //{
-        //    //this.rBody.angularVelocity = Vector2.zero;
-        //    this.rBody.velocity = Vector2.zero;
-        //    this.transform.localPosition = new Vector2(0, 0);
-        //}
-
-
-
-        //Target.localPosition = new Vector2(Random.value * 8 - 4,
-
-        //                                   Random.value * 8 - 4);
+        distance2Target= Vector2.Distance(this.transform.localPosition, Target.localPosition);
     }
 
     public float forceMultiplier = 10;
@@ -55,46 +40,35 @@ public class PlayerAgent : Agent
     {
         characterState.horizontal = actions.ContinuousActions[0];
         Vector2 controlSignal = Vector2.zero;
+
         controlSignal.x = actions.ContinuousActions[0];
-        //controlSignal.y = actionBuffers.ContinuousActions[1];
+        controlSignal.y = actions.ContinuousActions[1];
+
         rBody.AddForce(controlSignal * forceMultiplier);
 
-        float distanceToTarget = Vector2.Distance(this.transform.localPosition, Target.localPosition);
-        // reached target
-        //if (distanceToTarget < .42f)
-        //{
-        //   SetReward(1.0f);
-        //   EndEpisode();
-        //}
-        // died
-        //else if (this.transform.localPosition.y < 0)
-        //{
-        //    EndEpisode();
-        //}
-    }
+        float newDistance2Target = Vector2.Distance(this.transform.localPosition, Target.localPosition);
+        if (newDistance2Target < distance2Target) { AddReward(0.5f); }
+        else { AddReward(-0.5f); }
 
-    //public override void Heuristic(in ActionBuffers actionsOut)
-    //{
-    //    var continuousActionsOut = actionsOut.ContinuousActions;
-    //    continuousActionsOut[0] = Input.GetAxis("Horizontal");
-    //    continuousActionsOut[1] = Input.GetAxis("Vertical");
-    //}
+        distance2Target = newDistance2Target;
+    }
 
     public override void Heuristic(in ActionBuffers actionsOut)
     {
         ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
         continuousActions[0] = Input.GetAxisRaw("Horizontal");
+        continuousActions[1] = Input.GetAxisRaw("Vertical");
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.name.Equals("Target"))
+        if (collision.gameObject.name.Equals("Key"))
         {
             Debug.Log("Target reached!");
             SetReward(+20f);
             EndEpisode();
         }
-        else if (collision.gameObject.name.Equals("Crystal_01"))
+        else if (collision.gameObject.name.Equals("Spikes"))
         {
             Debug.Log("Agents Heaven reached!");
             SetReward(-20f);
@@ -110,10 +84,6 @@ public class PlayerAgent : Agent
         sensor.AddObservation(Target.localPosition);
         sensor.AddObservation(Death.localPosition);
         
-
-        // Agent velocity
-        //sensor.AddObservation(rBody.velocity.x);
-        //sensor.AddObservation(rBody.velocity.y);
     }
 
 
